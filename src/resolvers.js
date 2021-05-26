fec94d10a8b19ea2b1a00dc566651e833f47262e
Gustavo Santana <gustavo_santana_jussi@whirlpool.com>
2021-05-26 14:35:26 -0300

    Modulo 1 finalizado

--- src/resolvers.js
+++ src/resolvers.js
@@ -2,5 +2,11 @@ const { models } = require("./db");
 module.exports = {
     Query: {
         movies: () => models.Movie.findAll(),
+        getMovieByName: (_, args) => models.Movie.findOne(args),
+        users: () => models.User.findAll(),
+        getUserByName: (_, args) => models.User.findOne(args)
     },
+    Mutation: {
+        createUser: (_, { data }) => models.User.create(data)
+    }
 };
